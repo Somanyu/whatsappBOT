4065eb529ffc7f458220930614b62731072108e4
Somanyu <somanyu.03samal@gmail.com>
2021-04-08 18:32:09 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -12,9 +12,14 @@ def sms_reply():
     msg = request.form.get('Body')
 
     resp = MessagingResponse()
-    resp.message("Hi, I am friday personal assistant of Somanyu. Your message: *{}*".format(msg))
-    if (msg == "How are you??"):
+    # resp.message("Hi, I am friday personal assistant of Somanyu. Your message: *{}*".format(msg))
+    # if (msg == "How are you??"):
+    #     resp.message("Somanyu is doing good. He might know how are you doing?")
+    if(msg == "Hii"):
+        resp.message("Hi, I am friday personal assistant of Somanyu. Your message: *{}*".format(msg))
+    elif(msg == "How are you??"):
         resp.message("Somanyu is doing good. He might know how are you doing?")
+        
 
     return str(resp)
 
