277c728532c39a81fa946713badeaf68fd2a7e20
Somanyu <somanyu.03samal@gmail.com>
2021-04-08 18:37:25 +0530

    Added extra features

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def sms_reply():
     msg = request.form.get('Body')
 
     resp = MessagingResponse()
-    # resp.message("Hi, I am friday personal assistant of Somanyu. Your message: *{}*".format(msg))
-    # if (msg == "How are you??"):
-    #     resp.message("Somanyu is doing good. He might know how are you doing?")
+
     if(msg == "Hii"):
         resp.message("Hi, I am friday personal assistant of Somanyu. Your message: *{}*".format(msg))
     elif(msg == "How are you??"):
         resp.message("Somanyu is doing good. He might know how are you doing?")
-        
+    elif(msg == "What he is doing now?"):
+        resp.message("He is probably doing something incredible *;)*")
+
 
     return str(resp)
 
