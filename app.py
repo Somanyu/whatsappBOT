55769c4bc2eb5fd4af9adb76006dc7e3f096efcb
Somanyu <somanyu.03samal@gmail.com>
2021-04-09 20:12:58 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ def sms_reply():
 
     if(msg == "Hii" or msg == "hii"):
         resp.message("Hi, I am ```Proton``` personal assistant of Somanyu. Your message: *{}*".format(msg))
-        resp.message("I will convey your message ASAP!")
-        resp.message("Anything else you want to say to Somanyu?")
     elif(msg == "How are you?" or msg == "how are you?" or msg == "How are you"):
         resp.message("Somanyu is doing good. He might know how are you doing?")
     elif(msg == "What is he doing now?"):
